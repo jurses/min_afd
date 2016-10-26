ffc44589c49b4ec3485cf579eb7ceece16c6f9e7
jurses <martin.jurses@gmail.com>
2016-10-26 14:37:53 +0100

    adicion del dfa

--- dfa.cpp
+++ dfa.cpp
@@ -2,4 +2,16 @@
 namespace CYA{
 	dfa_t::dfa_t(void){
 	}
+
+	dfa_t::lee(std::ifstream& ifs){
+        if(!ifs.is_open()){
+            std::cerr << "Error al abrir el archivo" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        ifs >> n_est_;
+        cjt_est_.resize(n_est_);
+        for()
+        ifs >> id_inic_;
+
+	}
 }
--- dfa.hpp
+++ dfa.hpp
@@ -1,11 +1,20 @@
 #ifndef _DFA_H_
 #define _DFA_H_
+#include <vector>
+#include <ifstream>
+#include <cstdlib>
+#include "estado.hpp"
 namespace CYA{
 	class dfa_t{
 		private:
 			void eliminar_dfa(void);
+			std::vector<estado_t*> cjt_est_;
+			estado_t* est_inic_;
+			int n_est_;
+			int id_inic_;
 		public:
 			dfa_t(void);
+			void lee(std::ifstream&);
 			~dfa_t(void);
 	};
 }
